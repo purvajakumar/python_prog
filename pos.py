427d4b2bb04a0b6594887967849376c2f3c0e010
purvajakumar <53596547+purvajakumar@users.noreply.github.com>
2019-08-02 14:13:26 +0530

    Create pos.py

---
+++ pos.py
@@ -0,0 +1,13 @@
+#check whether the nmuber is postive or not
+n=int(input("Enter the value of n"))
+if(n<0):
+    print("negative number")
+elif(n>0):
+    print("positive number")
+else:
+    print("The number is zero")
+	
+    #output
+	"""Enter the value of n
+	   6
+       positive number"""
