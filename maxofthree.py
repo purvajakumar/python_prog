ae0fd165f7da1a3a845fba18e2050c3a401d649e
purvajakumar <53596547+purvajakumar@users.noreply.github.com>
2019-08-02 14:39:16 +0530

    Create maxofthree.py

---
+++ maxofthree.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the value of A"))
+b=int(input("Enter the value of B"))
+c=int(input("Enter the value of c"))
+def great(x,y,z):
+    return max(x,y,z)
+print("the largest number is ",great(a,b,c))
